e81d0d07b1b847d47f330f37bd68bc3aab4493d9
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-11-29 10:18:46 +0100

    Refactoring: move functions into BooruParser class.

--- lib/gelbooru_parser.py
+++ lib/gelbooru_parser.py
@@ -2,51 +2,57 @@ import re
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+class BooruView:
+    """Class for parsing a view and mining useful data."""
 
-def parse_tags(view):
-    """Return all tags of the given view."""
-    tags = view.select("li.tag-type-general > a")
-    tags = filter(lambda tag: tag != "?", (tag.contents[0] for tag in tags))
-    return list(tags)
+    def __init__(self, soup):
+        """Contructor: Takes a BeautifulSoup object for further parsing."""
+        self.soup = soup
 
-
-def parse_stats(view):
-    """Return the statistics of the given view."""
-    stats = view.select("div.sidebar3 > div#stats > ul > li")
-    stats = (line.contents for line in stats)
-    # Flatten stats. (list of list of strings -> list of strings)
-    stats = (item for sublist in stats for item in sublist)
-    stats = filter(lambda item: isinstance(item, str), stats)
-    regex = re.compile(r"""\w+(?=:)""")  # Check if "BLAH:", match only BLAH
-    statlist = []
-    for entry in stats:
-        key = regex.match(entry)
-        if not key:
-            break
-        key = key.group()
-        if key == "Id":
-            statlist.append(key)
-            statlist.append(int(entry[4:]))
-        elif key == "Posted":
-            statlist.append(key)
-            re_datetime = re.compile(r"""[\- :]""")  # Get date and time.
-            param_datetime = map(int, re_datetime.split(entry[8:]))
-            obj_datetime = datetime(*param_datetime)
-            statlist.append(obj_datetime)
-        elif key == "Size":
-            statlist.append(key)
-            sizes = entry[6:].split("x")
-            statlist.extend(sizes)
-        elif key == "Rating":
-            statlist.append(key)
-            statlist.append(entry[8:])
-    return statlist
+    def parse_tags(self):
+        """Return all tags of the view."""
+        tags = self.soup.select("li.tag-type-general > a")
+        tags = filter(lambda tag: tag != "?", (tag.contents[0] for tag in tags))
+        return list(tags)
 
 
-def parse_url(view):
-    """Return the URL of the original image."""
-    urls = view.select("div.sidebar3 > div > ul > li > a")
-    # Get tag containing the URL of the original image.
-    url = filter(lambda url: url.contents[0] == "Original image", urls)
-    url = list(url)[0]
-    return url.attrs["href"]
+    def parse_stats(self):
+        """Return the statistics of the view."""
+        stats = self.soup.select("div.sidebar3 > div#stats > ul > li")
+        stats = (line.contents for line in stats)
+        # Flatten stats. (list of list of strings -> list of strings)
+        stats = (item for sublist in stats for item in sublist)
+        stats = filter(lambda item: isinstance(item, str), stats)
+        regex = re.compile(r"""\w+(?=:)""")  # Check if "BLAH:", match only BLAH
+        statlist = []
+        for entry in stats:
+            key = regex.match(entry)
+            if not key:
+                break
+            key = key.group()
+            if key == "Id":
+                id_pair = (key, int(entry[4:]))
+                statlist.append(id_pair)
+            elif key == "Posted":
+                re_datetime = re.compile(r"""[\- :]""")  # Get date and time.
+                param_datetime = map(int, re_datetime.split(entry[8:]))
+                obj_datetime = datetime(*param_datetime)
+                date_pair = (key, obj_datetime)
+                statlist.append(date_pair)
+            elif key == "Size":
+                sizes = entry[6:].split("x")
+                size_triple = (key, sizes[0], sizes[1])
+                statlist.append(size_triple)
+            elif key == "Rating":
+                rating_pair = (key, entry[8:])
+                statlist.append(rating_pair)
+        return statlist
+
+
+    def parse_url(self):
+        """Return the URL of the original image."""
+        urls = self.soup.select("div.sidebar3 > div > ul > li > a")
+        # Get tag containing the URL of the original image.
+        url = filter(lambda url: url.contents[0] == "Original image", urls)
+        url = list(url)[0]
+        return url.attrs["href"]
