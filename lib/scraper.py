b1fbc5215a31eb78c624a19d39dd3b64eb832e01
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-11-29 22:13:11 +0100

    Added BooruList class + small refactorings

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,17 +1,27 @@
 import re
 from datetime import datetime
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+BOORU_URL = "http://gelbooru.com/"
+
 
 class BooruView:
+
     """Class for parsing a view and mining useful data."""
 
-    def __init__(self, soup):
-        """Contructor: Takes a BeautifulSoup object for further parsing."""
+    def __init__(self, soup, uid=None):
+        """
+        Contructor: Takes a BeautifulSoup object for further parsing.
+        If uid is given, assert if parsed id is equal.
+        """
         self.soup = soup
         self.tags = self._parse_tags()
         self.url = self._parse_url()
         stats = self._parse_stats()
         self.uid = self.__maybeIndex("Id", stats)
+        if uid:  # Safety check if you are parsing the correct view.
+            assert uid == self.uid
         self.posted = self.__maybeIndex("Posted", stats)
         self.rating = self.__maybeIndex("Rating", stats)
         sizes = self.__maybeIndex("Size", stats)
@@ -19,29 +29,26 @@ class BooruView:
             self.xsize = sizes[0]
             self.ysize = sizes[1]
 
-
     def __str__(self):
+        """Return a string that looks the same as the title of the original site."""
         signature = ""
         for tag in self.tags:
             signature += tag + " "
         signature += "- Image View -  | " + self.uid + " | Gelbooru"
         return signature
 
-
     def _parse_tags(self):
         """Return all tags of the view."""
         tags = self.soup.select("li.tag-type-general > a")
-        tags = filter(lambda tag: tag != "?", (tag.contents[0] for tag in tags))
+        tags = (tag.contents[0] for tag in tags if tag.contents[0] != "?")
         return list(tags)
-
 
     def _parse_stats(self):
         """Return the statistics of the view."""
         stats = self.soup.select("div.sidebar3 > div#stats > ul > li")
         stats = (line.contents for line in stats)
         # Flatten stats. (list of list of strings -> list of strings)
-        stats = (item for sublist in stats for item in sublist)
-        stats = filter(lambda item: isinstance(item, str), stats)
+        stats = (item for sublist in stats for item in sublist if isinstance(item, str))
         regex = re.compile(r"""\w+(?=:)""")  # Check if "BLAH:", match only BLAH
         # Match fitting lines in statistics element.
         matches = ((regex.match(entry), entry) for entry in stats if entry)
@@ -52,7 +59,7 @@ class BooruView:
             key = entry[0]
             value = entry[1]
             if key == "Id":
-                statdict[key] = value[4:]
+                statdict[key] = int(value[4:])
             elif key == "Posted":
                 re_datetime = re.compile(r"""[\- :]""")  # Get date and time.
                 param_datetime = map(int, re_datetime.split(value[8:]))
@@ -65,17 +72,57 @@ class BooruView:
                 statdict[key] = value[8:]
         return statdict
 
-
     def _parse_url(self):
         """Return the URL of the original image."""
         urls = self.soup.select("div.sidebar3 > div > ul > li > a")
         # Get tag containing the URL of the original image.
-        url = filter(lambda url: url.contents[0] == "Original image", urls)
-        url = list(url)[0]
-        return url.attrs["href"]
-    
+        return [url.attrs["href"] for url in urls if url.contents[0] == "Original image"][0]
+
     def __maybeIndex(self, key, dictionary):
         if key in dictionary:
             return dictionary[key]
         else:
             return None
+
+
+class BooruList:
+    """
+    Class for parsing one page of the listed results when querying the Booru.
+    You should be able to iterate over the object to get BooruView objects.
+    At the moment these should be at most 42.
+    """
+
+    def __init__(self, soup):
+        """Constructor: Takes a soup object of the results page."""
+        self.soup = soup
+        parsed_views = self._parse_links()
+        uids, urls = zip(*parsed_views)  # Unzip for better readability (Haha...)
+        soups = zip((BeautifulSoup(urlopen(BOORU_URL + url), "html.parser") for url in urls), uids)
+        # Create a list with views; soup[1] contains id for asserting correctness.
+        self._views = (BooruView(soup[0], soup[1]) for soup in soups)
+
+    def __iter__(self):
+        """For iterating through the BooruView objects."""
+        return self._views.__iter__()
+
+    def __next__(self):
+        """For iterating through the BooruView objects."""
+        return self._views.__next__()
+
+    def _parse_links(self):
+        """Returns a list of tumples (id, url) of all views of the page."""
+        links = self.soup.select("div#post-list > div.content > div > span.yup > span > a")
+        # Only includes objects with id attribute.
+        links = [(int(url.get("id")[1:]), url.get("href")) for url in links
+                 if url.get("id") is None]
+        return links
+
+
+class BooruQuery:
+    """
+    A class for getting all the pages of a query.
+    You should be able to iterate over the object to get the BooruList objects.
+    """
+
+    def __init__(self, soup):
+        self.soup = soup
