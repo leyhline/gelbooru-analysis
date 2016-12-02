12a0f975a1dd143784e44b84248be5a7fe28540d
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-03 00:17:47 +0100

    Adapted database.py to new schema. Renamed logfile.

--- src/database.py
+++ src/database.py
@@ -40,7 +40,7 @@ class BooruDB:
         self._cur.execute("PRAGMA foreign_keys = ON")
         self._cur.execute("PRAGMA foreign_keys")
         supported = self._cur.fetchone()
-        if supported == 1:
+        if supported and supported[0] == 1:
             return True
         else:
             return False
@@ -48,20 +48,46 @@ class BooruDB:
     def insert_view(self, booru_view):
         """Extracts data from BooruView object and writes it into database."""
         try:
+            # ENTITIES #
+            # Table for views and some specific statistics.
             val_view = (booru_view.uid, booru_view.posted, booru_view.score,
                         booru_view.xsize, booru_view.ysize, booru_view.url)
             self._cur.execute("INSERT OR REPLACE INTO view VALUES (?,?,?,?,?,?)", val_view)
-            val_tag = ((tag,) for tag in booru_view.tags)
+            tags, tag_types = zip(*booru_view.tagtuple)
+            # Table for listing all tags.
+            val_tag = ((tag,) for tag in tags)
             self._cur.executemany("INSERT OR IGNORE INTO tag(name) VALUES (?)", val_tag)
-            
-            
-            
-            
-            val_tagged = ((booru_view.uid, tag) for tag in booru_view.tags)
-            self._cur.executemany("INSERT OR IGNORE INTO tagged_with " +
-                                  "SELECT ?, id FROM tag WHERE name = ?", val_tagged)
+            # Table for listing all type of tags (like general, copyright, artist...).
+            val_tag_type = ((tag_type,) for tag_type in tag_types)
+            self._cur.executemany("INSERT OR IGNORE INTO tag_type(name) VALUES (?)", val_tag_type)
+            # Table for listing rating types (safe, questionable, explicit).
+            val_rating = (booru_view.rating,)
+            self._cur.execute("INSERT OR IGNORE INTO rating(name) VALUES (?)", val_rating)
+            # Table for listing all the people who posted pictures.
+            val_poster = (booru_view.poster,)
+            self._cur.execute("INSERT OR IGNORE INTO poster(name) VALUES (?)", val_poster)
+            # RELATIONSHIPS #
+            # Table for assigning tags to different views.
+            val_tags = ((booru_view.uid, tag) for tag in tags)
+            self._cur.executemany("INSERT OR IGNORE INTO tags " +
+                                  "SELECT ?, id FROM tag WHERE name = ?", val_tags)
+            # Table for assigning categories to tags.
+            val_categorizes = booru_view.tagtuple
+            self._cur.executemany("INSERT OR IGNORE INTO categorizes " +
+                                  "SELECT tag.id, tag_type.id FROM tag, tag_type " +
+                                  "WHERE tag.name = ? and tag_type.name = ?", val_categorizes)
+            # Table for assigning a poster to his/her view.
+            val_posts = (booru_view.poster, booru_view.uid)
+            self._cur.execute("INSERT OR REPLACE INTO posts " +
+                              "SELECT id, ? FROM poster WHERE name = ?", val_posts)
+            # Table for assigning a rating to a view.
+            val_rates = (booru_view.uid, booru_view.rating)
+            self._cur.execute("INSERT OR REPLACE INTO rates " +
+                              "SELECT ?, id FROM rating WHERE name = ?", val_rates)
             self._con.commit()
         except Exception as err:
             logging.critical("Aborting because of unhandled exception: " +
                               str(err) + " " + str(type(err)))
             raise err
+        finally:
+            logging.info("Inserting view {} into database.".format(booru_view.uid))
