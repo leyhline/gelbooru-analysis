075e44857555d3c7e80c07e6fc5866411424f53c
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-02 19:06:44 +0100

    Logging unhandled critical exceptions.

--- src/database.py
+++ src/database.py
@@ -2,7 +2,10 @@ import sqlite3
 import os.path
 
 STANDARD_DB_PATH = os.path.dirname(__file__) + "/../data/gelbooru.db"
-
+# Load and configure logging.
+with open("logging.yaml") as f:
+    logging_config = yaml.load(f)
+logging.config.dictConfig(logging_config)
 
 class BooruDB:
     """Class for accesing a SQLite database. Use per "with" statement."""
@@ -23,13 +26,17 @@ class BooruDB:
         self._con.close()
 
     def insert_view(self, booru_view):
-        """Extracts data from BooruView object and writes it into database."""
-        val_view = (booru_view.uid, booru_view.posted.isoformat(), booru_view.rating,
-                    booru_view.url, booru_view.xsize, booru_view.ysize)
-        self._cur.execute("INSERT OR REPLACE INTO view VALUES (?,?,?,?,?,?)", val_view)
-        val_tag = ((tag,) for tag in booru_view.tags)
-        self._cur.executemany("INSERT OR IGNORE INTO tag(name) VALUES (?)", val_tag)
-        val_tagged = ((booru_view.uid, tag) for tag in booru_view.tags)
-        self._cur.executemany("INSERT OR IGNORE INTO tagged_with " +
-                              "SELECT ?, id FROM tag WHERE name = ?", val_tagged)
-        self._con.commit()
+        try:
+            """Extracts data from BooruView object and writes it into database."""
+            val_view = (booru_view.uid, booru_view.posted.isoformat(), booru_view.rating,
+                        booru_view.url, booru_view.xsize, booru_view.ysize)
+            self._cur.execute("INSERT OR REPLACE INTO view VALUES (?,?,?,?,?,?)", val_view)
+            val_tag = ((tag,) for tag in booru_view.tags)
+            self._cur.executemany("INSERT OR IGNORE INTO tag(name) VALUES (?)", val_tag)
+            val_tagged = ((booru_view.uid, tag) for tag in booru_view.tags)
+            self._cur.executemany("INSERT OR IGNORE INTO tagged_with " +
+                                  "SELECT ?, id FROM tag WHERE name = ?", val_tagged)
+            self._con.commit()
+        except Exception as err:
+            logging.critical("Aborting because of unhandled exception: " + err + type(err))
+            raise err
