f0dd51bc32fff6bb39f262023d1576489d6a2752
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-02 22:01:03 +0100

    New, more complicated and hopefully better database schema

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -1,21 +1,52 @@
 CREATE TABLE view(
     id INTEGER PRIMARY KEY,
     posted TEXT,
-    rating TEXT,
+    score INTEGER,
     url TEXT,
     xsize INTEGER,
     ysize INTEGER
-);
+) WITHOUT ROWID;
 
 CREATE TABLE tag(
     id INTEGER PRIMARY KEY,
     name TEXT UNIQUE
 );
 
-CREATE TABLE  tagged_with(
-    view INTEGER,
-    tag INTEGER,
-    FOREIGN KEY(view) REFERENCES view(id),
-    FOREIGN KEY(tag) REFERENCES tag(id),
-    PRIMARY KEY (view, tag)    
+CREATE TABLE rating(
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE tag_type(
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE poster(
+    id INTEGER PRIMARY KEY,
+    name TEXT UNIQUE
+);
+
+CREATE TABLE tags(
+    view INTEGER REFERENCES view(id) ON DELETE CASCADE,
+    tag INTEGER REFERENCES tag(id) ON DELETE CASCADE,
+    PRIMARY KEY (view, tag)
+);
+
+CREATE TABLE categorizes(
+    tag INTEGER REFERENCES tag(id) ON DELETE CASCADE,
+    tag_type INTEGER REFERENCES tag_type(id),
+    PRIMARY KEY (tag, tag_type)
+);
+
+CREATE TABLE posts(
+    poster INTEGER REFERENCES poster(id),
+    view INTEGER REFERENCES view(id) ON DELETE CASCADE,
+    PRIMARY KEY (poster, view)
+);
+
+CREATE TABLE rates(
+    view INTEGER REFERENCES view(id) ON DELETE CASCADE,
+    rating INTEGER REFERENCES rating(id),
+    PRIMARY KEY (view, rating)
 );
