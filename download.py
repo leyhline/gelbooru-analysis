1c5b3ced5fe100852205609db55320c3a36b2a47
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-15 21:58:37 +0100

    Fixed bug with exception handling, made it more general and moved retrieving of image to own function.

--- download.py
+++ download.py
@@ -8,9 +8,26 @@ EXCEPTION_LIMIT = 10
 logging.basicConfig(filename="download.log", filemode="w", level=logging.INFO,
                     format = "%(asctime)s - %(levelname)s - %(message)s")
 
+
+def retrieve_image(url, dest_path):
+    if not os.path.exists(dest_path):
+        try:
+            urlretrieve(url, dest_path)
+        except urllib.error.URLError as err:
+            logging.error("Retrieving image failed: " + str(err))
+            exception_counter += 1
+            if exception_counter > EXCEPTION_LIMIT:
+                logging.critical("Retrieving images failed more than {} times. Aborting.".format(EXCEPTION_LIMIT))
+                raise(err)
+            else:
+                return
+    else:
+        logging.info("File already exists: {}".format(dest_path))
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        logging.error("No argument given. Usage: {} FILENAME".format(sys.argv[0]))
+        print("No argument given. Usage: {} FILENAME".format(sys.argv[0]))
         sys.exit(1)
     inputpath = sys.argv[1]
     if not os.path.exists(inputpath):
@@ -29,14 +46,4 @@ if __name__ == "__main__":
             dest_file = str(uid) + os.path.splitext(url)[1]
             dest_path = dest_folder + "/" + dest_file
             logging.info("Retrieving image {} from {}".format(uid, url))
-            if not os.path.exists(dest_path):
-                try:
-                    urlretrieve(url, dest_path)
-                except urllib.error.HTTPError as err:
-                    logging.error("Retrieving image {} failed: ".format(uid) + str(err))
-                    exception_counter += 1
-                    if exception_counter > EXCEPTION_LIMIT:
-                        logging.critical("Retrieving images failed more than {} times. Aborting.".format(EXCEPTION_LIMIT))
-                        raise(err)
-            else:
-                logging.info("File already exists: {}".format(dest_path))
+            retrieve_image(url, dest_path)
