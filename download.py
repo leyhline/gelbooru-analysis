073f53de6e61ca80e79948a4d3b3546492aa7a66
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-15 22:28:35 +0100

    Outsourcing code to new function.

--- download.py
+++ download.py
@@ -5,6 +5,7 @@ import sys
 from urllib.request import urlretrieve
 
 EXCEPTION_LIMIT = 10
+exception_counter = 0
 logging.basicConfig(filename="download.log", filemode="w", level=logging.INFO,
                     format = "%(asctime)s - %(levelname)s - %(message)s")
 
@@ -14,7 +15,11 @@ def retrieve_image(url, dest_path):
         try:
             urlretrieve(url, dest_path)
         except urllib.error.URLError as err:
-            logging.error("Retrieving image failed: " + str(err))
+            if hasattr(err, "reason"):
+                logging.error("Failed to reach server: " + str(err.reason))
+            elif hasattr(err, "code"):
+                logging.error("Server could not fulfill request: " + str(err.code))
+            global exception_counter
             exception_counter += 1
             if exception_counter > EXCEPTION_LIMIT:
                 logging.critical("Retrieving images failed more than {} times. Aborting.".format(EXCEPTION_LIMIT))
@@ -23,6 +28,13 @@ def retrieve_image(url, dest_path):
                 return
     else:
         logging.info("File already exists: {}".format(dest_path))
+
+
+def download_images(url, uid, dest_folder):
+    dest_file = str(uid) + os.path.splitext(url)[1]
+    dest_path = dest_folder + "/" + dest_file
+    logging.info("Retrieving image {} from {}".format(uid, url))
+    retrieve_image(url, dest_path)
 
 
 if __name__ == "__main__":
@@ -40,10 +52,6 @@ if __name__ == "__main__":
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
     with open(inputpath) as f:
-        exception_counter = 0
         for line in f:
             url, uid = line.split()
-            dest_file = str(uid) + os.path.splitext(url)[1]
-            dest_path = dest_folder + "/" + dest_file
-            logging.info("Retrieving image {} from {}".format(uid, url))
-            retrieve_image(url, dest_path)
+            download_images(url, uid, dest_folder)
