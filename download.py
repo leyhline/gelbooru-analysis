e0038a9192429c7098d587997195e4f52b67074c
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-15 22:42:02 +0100

    Added concurrency.

--- download.py
+++ download.py
@@ -2,9 +2,11 @@ import os
 import logging
 import urllib.error
 import sys
+import concurrent.futures
 from urllib.request import urlretrieve
 
 EXCEPTION_LIMIT = 10
+MAX_WORKERS = 4
 exception_counter = 0
 logging.basicConfig(filename="download.log", filemode="w", level=logging.INFO,
                     format = "%(asctime)s - %(levelname)s - %(message)s")
@@ -27,10 +29,11 @@ def retrieve_image(url, dest_path):
             else:
                 return
     else:
-        logging.info("File already exists: {}".format(dest_path))
+        logging.warning("File already exists: {}".format(dest_path))
 
 
-def download_images(url, uid, dest_folder):
+def download_images(list_entry, dest_folder):
+    url, uid = list_entry.split()
     dest_file = str(uid) + os.path.splitext(url)[1]
     dest_path = dest_folder + "/" + dest_file
     logging.info("Retrieving image {} from {}".format(uid, url))
@@ -43,7 +46,7 @@ if __name__ == "__main__":
         sys.exit(1)
     inputpath = sys.argv[1]
     if not os.path.exists(inputpath):
-        logging.error("{} does not exist.".format(inputpath))
+        print("{} does not exist.".format(inputpath))
         sys.exit(1)
     pathname = os.path.abspath(inputpath)
     pathname, filename = os.path.split(pathname)
@@ -51,7 +54,11 @@ if __name__ == "__main__":
     dest_folder = pathname + "/" + basename
     if not os.path.exists(dest_folder):
         os.mkdir(dest_folder)
+    # Read file contents to list.
+    url_list = []
     with open(inputpath) as f:
         for line in f:
-            url, uid = line.split()
-            download_images(url, uid, dest_folder)
+            url_list.append(line)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS) as e:
+        for i in range(len(url_list)):
+            e.submit(download_images, url_list[i], dest_folder)
