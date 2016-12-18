f2d602c41640c984591d40f3e1ec207c9077236e
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2016-12-18 10:35:43 +0100

    verify_download.sh checks now for differences.

--- verify_download.sh
+++ verify_download.sh
@@ -11,6 +11,14 @@ current_count=$(ls $path | wc -w)
 target_count=$(cat $source_file | wc -l)
 echo Current number of images: $current_count
 echo Total number of images:   $target_count
+
+if [ $current_count -ne $target_count ]; then
+	ist=$(ls ${path} | grep -o '[0-9]\+')
+	soll=$(grep -o ' [0-9]\+' ${source_file})
+	echo Missing files:
+	echo ${soll[@]} ${ist[@]} | tr ' ' '\n' | sort -n | uniq -u
+fi
+
 echo Checking for corrupted images...
 for file in $(ls $path); do
 	gm identify ${path}/${file} > /dev/null
